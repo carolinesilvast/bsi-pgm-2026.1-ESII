0da9c3d06fb9a34f0b0fe6efe3f0e2f5dbd2e2c7
carolinesilvast <caroline.silvaa605@gmail.com>
2026-05-20 21:10:15 -0300

    Create equipamento.py

---
+++ models/equipamento.py
@@ -0,0 +1,10 @@
+# Equipamento: representar os dados de um equipamento.
+
+from dataclasses import dataclass
+
+@dataclass
+class Equipamento:
+    id: int
+    nome: str
+    tipo: str
+    disponivel: bool = True
