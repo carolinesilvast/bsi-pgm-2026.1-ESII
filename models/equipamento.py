c50a5254a680e7613e427ba764669d5d435fd0f1
carolinesilvast <caroline.silvaa605@gmail.com>
2026-05-20 21:20:08 -0300

    Update equipamento.py

--- models/equipamento.py
+++ models/equipamento.py
@@ -1,10 +1,37 @@
-# Equipamento: representar os dados de um equipamento.
+# Equipamento: representar os dados e calcular multa.
 
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
 
 @dataclass
-class Equipamento:
+class Equipamento(ABC):
     id: int
     nome: str
     tipo: str
     disponivel: bool = True
+
+    @abstractmethod
+    def calcular_multa(self, atraso):
+        pass
+
+
+@dataclass
+class Notebook(Equipamento):
+
+    def calcular_multa(self, atraso):
+        return max(0, atraso * 10.0)
+
+
+@dataclass
+class Projetor(Equipamento):
+
+    def calcular_multa(self, atraso):
+        return max(0, atraso * 15.0)
+
+
+@dataclass
+class Cabo(Equipamento):
+
+    def calcular_multa(self, atraso):
+        return max(0, atraso * 2.0)
