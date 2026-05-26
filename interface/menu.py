33c3810e3047ee96a0cc72f6671f1a083ac212f6
carolinesilvast <caroline.silvaa605@gmail.com>
2026-05-26 15:07:08 -0300

    Update menu.py

--- interface/menu.py
+++ interface/menu.py
@@ -1,20 +1,25 @@
 # Menu: realizar interação com o usuário.
 
-from services.servico_emprestimo import ServicoEmprestimo
-
-
 class Menu:
 
-    def __init__(self):
-        self.servico = ServicoEmprestimo()
+    def __init__(self, servico):
+        self.servico = servico
 
     def executar(self):
+
         while True:
-            print("\n1-Registrar  2-Devolver  3-Atrasados  0-Sair")
+
+            print(
+                "\n1-Registrar  "
+                "2-Devolver  "
+                "3-Atrasados  "
+                "0-Sair"
+            )
 
             op = input("Opção: ")
 
             if op == "1":
+
                 self.servico.registrar(
                     int(input("ID equipamento: ")),
                     input("Nome: "),
@@ -23,11 +28,13 @@ class Menu:
                 )
 
             elif op == "2":
+
                 self.servico.devolver(
                     int(input("ID empréstimo: "))
                 )
 
             elif op == "3":
+
                 self.servico.listar_atrasados()
 
             elif op == "0":
