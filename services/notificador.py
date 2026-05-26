9cf37ba46a9501956c02edfe56a72dd1ed34e135
carolinesilvast <caroline.silvaa605@gmail.com>
2026-05-26 15:05:33 -0300

    Update notificador.py

--- services/notificador.py
+++ services/notificador.py
@@ -2,5 +2,31 @@
 
 class Notificador:
 
-    def enviar(self, email, mensagem):
-        print(f"[EMAIL] {email} — {mensagem}")
+    def notificar_emprestimo(
+        self,
+        email,
+        data_devolucao
+    ):
+        print(
+            f"[EMAIL] {email} — "
+            f"empréstimo até {data_devolucao}"
+        )
+
+    def notificar_devolucao(
+        self,
+        email,
+        multa
+    ):
+        print(
+            f"[EMAIL] {email} — "
+            f"multa R${multa:.2f}"
+        )
+
+    def notificar_atraso(
+        self,
+        email
+    ):
+        print(
+            f"[EMAIL] {email} — "
+            f"você está em atraso!"
+        )
