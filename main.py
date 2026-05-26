d19a1e1fec2a8a4fc9db0e7c490a8b5c9975cf6b
carolinesilvast <caroline.silvaa605@gmail.com>
2026-05-26 15:06:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-#O menu será implementado depois.
+# main: iniciar o sistema.
+
+from interface.menu import Menu
+
+from repositories.repositorio_emprestimo import (
+    RepositorioEmprestimo
+)
+
+from services.notificador import Notificador
+
+from services.servico_emprestimo import (
+    ServicoEmprestimo
+)
+
+
+def main():
+
+    repositorio = RepositorioEmprestimo()
+
+    notificador = Notificador()
+
+    servico = ServicoEmprestimo(
+        repositorio,
+        notificador
+    )
+
+    menu = Menu(servico)
+
+    menu.executar()
+
+
+if __name__ == "__main__":
+    main()
