1bf894f7a94cd475bd9af8bd2a2af4ed6a5a10ed
carolinesilvast <caroline.silvaa605@gmail.com>
2026-06-09 22:27:21 -0300

    Create test_multa.py

---
+++ multa.py tests/test_multa.py
@@ -0,0 +1,8 @@
+from multa import calcular_multa_com_carencia
+
+def test_multa_zero_quando_sem_atraso():
+    assert calcular_multa_com_carencia(
+        dias_atraso=0,
+        dias_carencia=2,
+        valor_por_dia=10.0
+    ) == 0.0
