d40cf952706c76d7266c08a22790d2587e3fdc05
carolinesilvast <caroline.silvaa605@gmail.com>
2026-06-09 23:20:58 -0300

    Update limite_emprestimos.py

--- limite_emprestimos.py
+++ limite_emprestimos.py
@@ -1,10 +1,15 @@
+def atingiu_limite(
+    emprestimos_abertos,
+    limite
+):
+    return emprestimos_abertos >= limite
+
+
 def pode_realizar_emprestimo(
     emprestimos_abertos,
     limite
 ):
-    return True
-def pode_realizar_emprestimo(
-    emprestimos_abertos,
-    limite
-):
-    return emprestimos_abertos < limite
+    return not atingiu_limite(
+        emprestimos_abertos,
+        limite
+    )
