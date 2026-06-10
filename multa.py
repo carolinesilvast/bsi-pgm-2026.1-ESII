ba3c0c91862acc270640772ac50fafaf3e0cce32
carolinesilvast <caroline.silvaa605@gmail.com>
2026-06-09 22:54:02 -0300

    Update multa.py

--- multa.py
+++ multa.py
@@ -3,4 +3,6 @@ def calcular_multa_com_carencia(
     dias_carencia,
     valor_por_dia
 ):
-    return 0.0
+    dias_excedentes = dias_atraso - dias_carencia
+
+    return dias_excedentes * valor_por_dia
