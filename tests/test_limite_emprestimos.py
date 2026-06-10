3005fff649a3002faab8a6c4642824fa099602ea
carolinesilvast <caroline.silvaa605@gmail.com>
2026-06-09 23:15:38 -0300

    Update test_limite_emprestimos.py

--- tests/test_limite_emprestimos.py
+++ tests/test_limite_emprestimos.py
@@ -1,1 +1,8 @@
+from limite_emprestimos import pode_realizar_emprestimo
 
+
+def test_usuario_abaixo_do_limite_pode_emprestar():
+    assert pode_realizar_emprestimo(
+        emprestimos_abertos=2,
+        limite=3
+    ) is True
