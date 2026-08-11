bca0d6ac4fed2ca9eb5ab9ba02ea6e1c5debeb42
carolinesilvast <caroline.silvaa605@gmail.com>
2026-08-11 20:12:05 -0300

    Create emprestimo.py

---
+++ models/emprestimo.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class Emprestimo:
+    id: int
+    equipamento_id: int
+    usuario_nome: str
+    usuario_email: str
+    data_emprestimo: date
+    data_devolucao: date
+    devolvido: bool = False
