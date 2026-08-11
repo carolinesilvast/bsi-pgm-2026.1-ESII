5f18022321d4e163ce8d1b45417357b6dedc68e4
carolinesilvast <caroline.silvaa605@gmail.com>
2026-08-11 20:15:49 -0300

    Create notificador_email.py

---
+++ services/notificador_email.py
@@ -0,0 +1,12 @@
+from services.evento import Evento
+from services.observer import Observer
+
+
+class NotificadorEmail(Observer):
+    def update(self, evento: Evento) -> None:
+        if evento.tipo == "emprestimo":
+            print(f"[EMAIL] {evento.email} - empréstimo até {evento.data}")
+        elif evento.tipo == "devolucao":
+            print(f"[EMAIL] {evento.email} - multa R${evento.multa:.2f}")
+        elif evento.tipo == "atraso":
+            print(f"[EMAIL] {evento.email} - você está em atraso!")
