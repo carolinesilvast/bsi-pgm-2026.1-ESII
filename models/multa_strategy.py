f9c70cb383199fe7e28f81b65ca3a5ed08bf5464
carolinesilvast <caroline.silvaa605@gmail.com>
2026-08-11 20:12:52 -0300

    Update multa_strategy.py

--- models/multa_strategy.py
+++ models/multa_strategy.py
@@ -1,16 +1,26 @@
 from abc import ABC, abstractmethod
 
-class MultaStrategy(ABC):
 
+class MultaStrategy(ABC):
     @abstractmethod
     def calcular(self, dias_atraso: int) -> float:
         pass
 
 
 class MultaPorDia(MultaStrategy):
-
     def __init__(self, valor_dia: float):
         self.valor_dia = valor_dia
 
     def calcular(self, dias_atraso: int) -> float:
         return max(0.0, dias_atraso * self.valor_dia)
+
+
+class MultaProgressiva(MultaStrategy):
+    def calcular(self, dias_atraso: int) -> float:
+        if dias_atraso <= 0:
+            return 0.0
+        if dias_atraso <= 3:
+            return dias_atraso * 5.0
+        if dias_atraso <= 7:
+            return dias_atraso * 10.0
+        return dias_atraso * 20.0
