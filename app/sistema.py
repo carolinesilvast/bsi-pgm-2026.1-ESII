4bed4ea2954349a913ca99ff062b877cc739285f
carolinesilvast <caroline.silvaa605@gmail.com>
2026-08-11 20:10:31 -0300

    Update sistema.py

--- app/sistema.py
+++ app/sistema.py
@@ -1,26 +1,18 @@
 from repositories.repositorio_emprestimo import RepositorioEmprestimo
-from services.notificador import Notificador
+from services.notificador_email import NotificadorEmail
 from services.servico_emprestimo import ServicoEmprestimo
 
 
 class SistemaDeEmprestimos:
-    """Fachada: esconde a montagem do subsistema e expõe operações simples."""
+    """Facade: esconde a montagem do subsistema e expõe operações simples."""
 
     def __init__(self):
         self._repositorio = RepositorioEmprestimo()
-        self._notificador = Notificador()
-        self._servico = ServicoEmprestimo(
-            self._repositorio,
-            self._notificador
-        )
+        self._servico = ServicoEmprestimo(self._repositorio)
+        self._servico.registrar_observer(NotificadorEmail())
 
     def registrar(self, equipamento_id, nome, email, dias):
-        return self._servico.registrar(
-            equipamento_id,
-            nome,
-            email,
-            dias
-        )
+        return self._servico.registrar(equipamento_id, nome, email, dias)
 
     def devolver(self, emprestimo_id):
         return self._servico.devolver(emprestimo_id)
